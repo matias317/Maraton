eba417b64326470e5070cd483dcd9d40f49b4242
Cristian Ospina <ospinaospinacristian@gmail.com>
2018-12-03 19:58:45 -0500

    Se agregan estilos y funcionalidad con jquery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
 
+    $("#colorCamisa").css('background-color', "blue");
+
     $("#inscribirse").click(function(){
         window.location.replace("formulario.html");
     });
 
+    $("#selectColor").change(function(){
+        var color = $("#selectColor option:selected").val();
+        $("#colorCamisa").css('background-color', color);
+    });
+
  });
+
 
